78b954dacc29fa55db382e48197945e1573a2e3a
Roger Elliot Witkin <roger@lnxsrv04.seas.ucla.edu>
2015-05-25 02:48:30 -0700

    read-dv

---
+++ read-dv.cpp
@@ -0,0 +1,31 @@
+#include <list>
+#include <string>
+#include <utility>
+#include <cstdlib>
+
+void get_lcp(const std::string &lcpinfo, std::list<std::pair<std::string, int> > &lcplist)
+{
+  
+  int start_index = 0;
+  int lcp_num;
+  std::string name;
+  std::string lcp;
+  
+  for (int i = 0; i < lcpinfo.length(); i++)
+    {
+      if (lcpinfo[i] == 0)
+	{
+	  name = lcpinfo.substr(start_index, i - start_index);
+	  lcp = lcpinfo.substr(i + 1, 4);
+
+	  lcp_num = lcp[0] << 24 + lcp[1] << 16 + lcp[2] << 8 + lcp[3];
+	  
+	  std::pair<std::string, int> val_pair;
+	  val_pair = std::make_pair( name, lcp_num );
+	  lcplist.push_back(val_pair);
+
+	  start_index = i + 5;
+	}
+    }
+}
+
